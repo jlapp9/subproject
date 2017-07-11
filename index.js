f27c214f53778f80c85d1696455c64524f7643e6
jlapp9 <jameson@jamesonlapp.com>
2017-07-11 09:10:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 exports.printMsg = function() {
-  console.log("This is a message from the sub package.  Updated 2017-07-11 09:09");
+  console.log("This is a message from the sub package.  Updated 2017-07-11 09:10");
 }
